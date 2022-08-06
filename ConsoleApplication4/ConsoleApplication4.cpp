2db34b49198131653df9581d63dfa1b216726f32
mateu <mateu@DESKTOP-BL2E1H5>
2022-08-06 20:40:53 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>     // std::cout
+#include <fstream> 
+#include <vector>
+                    // std::ifstream
+using namespace std;
+
+int main() {
+
+    ifstream ifs("test.txt", ifstream::in);
+
+    string input;
+    vector<string> names;
+    while (ifs >> input ) 
+    {
+        int pos = input.find(":");
+        string sub = input.substr(pos + 1);
+        names.push_back(input);
+        cout << sub;
+    }
+
+  
+
+   
+
+    return 0;
+
+   
+}
